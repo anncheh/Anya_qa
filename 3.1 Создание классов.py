2df682b4d1b70eb3d4cba0fbf526193a015672ef
anna.ch@ati.su <anna.ch@ri.domain>
2025-06-01 23:04:55 +0300

    добавление файлов x

--- 3.1 Создание классов.py
+++ 3.1 Создание классов.py
@@ -20,3 +20,23 @@ woman = Person('Настя',28)
 
 man.sing()
 woman.dance()
+
+class Animal():
+    def __init__(self, type, name, age, bridd):
+        self.type = type
+        self.name = name
+        self.age = age
+        self.bridd = bridd
+    def eat(self):
+        print(self.type + " " + self.name + ' ест')
+    def drink(self):
+        print(self.type + ' ' + self.name + " пьет")
+    def walk(self):
+        print(self.type + ' ' + self.name + " гуляет")
+
+cow = Animal("Корова", 'Мася', 10, 'татарская')
+dog = Animal("Собака", 'Чика', 4, 'чихуа')
+cow.eat()
+dog.eat()
+dog.drink()
+dog.walk()
